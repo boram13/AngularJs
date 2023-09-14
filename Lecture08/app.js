a3fcec1e7f97a1b90e26beff2d25eeec5127c538
boram13 <bmenerja@helius-systems.com>
2023-09-14 14:50:36 +0200

    first lessons in angular

--- Lecture08/app.js
+++ Lecture08/app.js
@@ -3,18 +3,17 @@
     
     angular.module('NameCalculator', [])
     
-    .controller('NameCalculatorController', function ($scope) {
+    .controller('NameCaculatorController', function ($scope) {
       $scope.name = "";
-      $scope.totalValue = 0; //initial 0
+      $scope.totalValue = 0;
     
       $scope.displayNumeric = function () {
-        //get the total value
-        var totalNameValue = calculateNumericForString($scope.name);
+        var totalNameValue = calculatNumericForString($scope.name);
         $scope.totalValue = totalNameValue;
       };
     
     
-      function calculateNumericForString(string) {
+      function calculatNumericForString(string) {
         var totalStringValue = 0;
         for (var i = 0; i < string.length; i++) {
           totalStringValue += string.charCodeAt(i);
