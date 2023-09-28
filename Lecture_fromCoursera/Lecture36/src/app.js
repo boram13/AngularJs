a443ef1f0fa981befdef352243915cc8cc48131a
boram13 <bmenerja@helius-systems.com>
2023-09-28 10:56:39 +0200

    routing and eouting state with controller

---
+++ Lecture_fromCoursera/Lecture36/src/app.js
@@ -0,0 +1,31 @@
+(function () {
+
+  //declare a dependency of our module called ui....
+    angular.module('RoutingApp',['ui.router']);
+    
+    angular.module('RoutingApp')
+    .config(RoutesConfig);
+    
+    //the providers are the one who are going to be configuring the state
+    //service and the url router  service 
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+    
+      // Redirect to tab 1 if no other URL matches
+      $urlRouterProvider.otherwise('/tab1');
+    
+      // Set up UI states
+      $stateProvider
+        .state('tab1', {
+          url: '/tab1',
+          templateUrl: 'src/tab1.html'
+        })
+    
+        .state('tab2', {
+          url: '/tab2',
+          templateUrl: 'src/tab2.html'
+        });
+    }
+    
+    
+    })();
